69d0a73166c819d93cef28b7a6b6cc267345dd68
Emil Iliev <emiliev1994@gmail.com>
2020-07-15 00:57:04 +0300

    Loggers, Builders, Algorithm optimization etc..

---
+++ Eigen_test/Eigen_test/Observer/ConsoleLogger.hpp
@@ -0,0 +1,14 @@
+//
+//  ConsoleLogger.hpp
+//  Eigen_test
+//
+//  Created by Emil Iliev on 18.02.20.
+//  Copyright © 2020 Emil Iliev. All rights reserved.
+//
+
+#ifndef ConsoleLogger_hpp
+#define ConsoleLogger_hpp
+
+#include <stdio.h>
+
+#endif /* ConsoleLogger_hpp */
---
+++ Eigen_test/Eigen_test/Observer/SerialLogger.hpp
@@ -0,0 +1,14 @@
+//
+//  SerialLogger.hpp
+//  Eigen_test
+//
+//  Created by Emil Iliev on 18.02.20.
+//  Copyright © 2020 Emil Iliev. All rights reserved.
+//
+
+#ifndef SerialLogger_hpp
+#define SerialLogger_hpp
+
+#include <stdio.h>
+
+#endif /* SerialLogger_hpp */
